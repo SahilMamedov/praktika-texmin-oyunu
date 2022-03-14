e3ae7b6991aaef2838a368bf116e9404968fa865
SahilMamedov <sahilim@code.edu.az>
2022-03-14 18:38:41 +0400

    praktika-oyunu

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace praktika_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int texmin=0;
+            int yaddash;
+            int deneme=0;
+            Random rastgele = new Random();
+            yaddash = rastgele.Next(1, 1000);
+            while (texmin != yaddash)
+            {
+                deneme++;
+                Console.WriteLine("Sayi yazin");
+                texmin = Convert.ToInt16(Console.ReadLine());
+
+                if (texmin > yaddash)
+                {
+                    Console.WriteLine(" Daha balaca say yazin");
+
+                }
+                else if (texmin < yaddash)
+                {
+                    Console.WriteLine("Daha boyuk say yazin");
+
+                }
+                else
+                {
+                    Console.WriteLine("Tebrikler");
+                    Console.WriteLine("{0}.Yoxlamada tapdiniz", deneme);
+                    Console.Read();
+                }
+               
+               
+            }
+        }
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"# praktika-texmin-oyunu" 
